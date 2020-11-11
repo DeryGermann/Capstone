253fa8b208d35154f2be9fe62c136cabf00cbd59
DeryGermann <termaiter@gmail.com>
2020-11-10 17:15:05 -0700

    Puzzle Logic
    Basic puzzle logic completed.

--- FrontEnd Server/src/components/puzzle.js
+++ FrontEnd Server/src/components/puzzle.js
@@ -10,7 +10,7 @@ class PuzzlePage extends Component {
 
         this.state = {
             copyright: 'Copyright Dery Germann 2020',
-            gbh: <a href='puzzle' id='go-back-home-button'><p>&#8592; Go Back To Your Account</p></a>,
+            gbh: <a href='account' id='go-back-home-button'><p>&#8592; Go Back To Your Account</p></a>,
             img_width: 0,
             img_height: 0,
             window_width: window.innerWidth,
@@ -55,7 +55,7 @@ class PuzzlePage extends Component {
 
         // console.log(canvas.width);
         // console.log(canvas.height);
-        console.log(this.state.window_width);
+        // console.log(this.state.window_width);
 
         // Create 25 divs with set styling and outlines
         for (let y = 0; y < 5; y++){
@@ -63,7 +63,7 @@ class PuzzlePage extends Component {
             for (let x = 0; x < 5; x++) {
                 puzzle_column.push(<div key={`${y},${x}`} style={
                     {
-                        border: '1px solid black',
+                        border: '1px solid grey',
                         maxHeight: canvas.height/5,
                         minHeight: canvas.height/5
                     }
@@ -78,6 +78,47 @@ class PuzzlePage extends Component {
         }
 
         // Create the 25 drag elements
+        let puzzle_width = canvas.width / 5;
+        let puzzle_height = canvas.height / 5; 
+        console.log(puzzle_width);
+        console.log(puzzle_height);
+        let y_counter = -1;
+        let x_counter = -1;
+        for (let y = 0; y < 5; y++) {
+            y_counter += 1;
+
+            x_counter = -1;
+            for (let x = 0; x < 5; x++) {
+                x_counter += 1;
+
+                let c = document.createElement('canvas');
+                let context = c.getContext('2d');
+                c.width = puzzle_width;
+                c.height = puzzle_height;
+
+                context.drawImage(canvas, 
+                    x_counter * puzzle_width,
+                    y_counter * puzzle_height,
+                    puzzle_width,
+                    puzzle_height,
+                    0, 0,
+                    puzzle_width,
+                    puzzle_height
+                );
+
+                drag_items.push(<Draggable bounds={{top: -65}}>
+                    <img src={c.toDataURL()} style={{border: '1px solid black'}}/>
+                </Draggable>);
+            }
+        }
+
+        // Fisher-Yates Algorithm
+        for(let i = drag_items.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * i)
+            const temp = drag_items[i]
+            drag_items[i] = drag_items[j]
+            drag_items[j] = temp
+        }
 
         return (
             <div id='pageContent'>
@@ -89,20 +130,22 @@ class PuzzlePage extends Component {
                     />
                 </div>
                 <div id='content'>
-                    <h2>{data[0].name}</h2>
+                    <h2>Puzzle Name: <u>{data[0].name}</u></h2>
 
-                    <div id='puzzle-view'>
-                        <div id='left-puzzle'>
-                            <Draggable bounds={{top: -65}}>
-                                <div className='button'>Something</div>
-                            </Draggable>
+                    <div id='solving-puzzle-view'>
+                        <div style={{display: 'flex', flexDirection: 'row'}}>
+                            <div id='puzzle-pieces'>
+                                <h2>Puzzle Pieces</h2>
+                                { drag_items }
+                            </div>
+                            <div id='puzzle-outline'>
+                                { puzzle_grid }
+                            </div>
                         </div>
-                        <div id='puzzle-outline'>
-                            { puzzle_grid }
-                        </div>
-                        <div id='right-puzzle'>
-                            <img src={canvas.toDataURL()}/>
-                        </div>
+                    </div>
+                    <div id='puzzle-preview'>
+                        <h2>What Your Puzzle Should Look Like!</h2>
+                        <img id='preview' src={canvas.toDataURL()}/>
                     </div>
                 </div>
                 <div id='footer'>
